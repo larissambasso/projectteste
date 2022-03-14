d6aee3e6be17fd9c4cafe1ae9afb84da9c686afc
larissambasso <larissa.mbasso@outlook.com>
2022-03-14 10:22:37 -0300

    14.03

---
+++ nometeste/src/components/Cards.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="card">
+    <img>
+    <h3>Card 1</h3>
+    <button>Clique aqui</button>
+  </section>
+</template>
+
+<script>
+  defineProps({
+    card:{
+
+    }
+  })
+</script>
+
+<style scoped>
+  img{
+    height: 100px;
+    width: auto;
+  }
+  .card{
+    background-color: black;
+  }
+</style>
+
--- nometeste/src/views/HomeView.vue
+++ nometeste/src/views/HomeView.vue
@@ -4,9 +4,9 @@ import MeuprimeiroComponente from '@/components/MeuprimeiroComponente.vue'
 
 <template>
   <main>
-    <MeuprimeiroComponente />
-    <MeuprimeiroComponente 
-      texto='da csdfcsewfcvjmsdvo fpoaifjm afasf asfacaSFEsf Daniel' 
+    <Cards card='oi'/>
+    <Cards 
+      img='da csdfcsewfcvjmsdvo fpoaifjm afasf asfacaSFEsf Daniel' 
       endereco='https://t5z6q4c2.rocketcdn.me/wp-content/uploads/2020/08/sol-afinal-qual-e-sua-cor-conceitos-e-caracteristicas.jpg'
     
     />
