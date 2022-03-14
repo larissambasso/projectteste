f837ae6b41feab69dcb959eaa0d1e72aab8d2f34
larissa moura basso <larissa.mbasso@outlook.com>
2022-03-13 23:45:35 -0300

    atualizando

---
+++ nometeste/src/components/MeuprimeiroComponente.vue
@@ -0,0 +1,29 @@
+<template>
+    <p :class="'ddd'===texto ? 'exemplo' : 'vermelho'">{{texto}}</p>
+    <img v-if="'larissa'!==endereco" :src='endereco'>
+</template>
+
+<script setup>
+    defineProps({
+        texto:{
+            type:String,
+            default: 'ddd'
+        },
+        endereco:{
+            type:String,
+            default: 'larissa'
+        }
+    })
+</script>
+<style scoped>
+    .exemplo{
+        color: green;
+    }
+    .vermelho{
+        color: red;
+    }
+    img{
+        height: 100px;
+        width: auto;
+    }
+</style>
--- nometeste/src/views/HomeView.vue
+++ nometeste/src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+import MeuprimeiroComponente from '@/components/MeuprimeiroComponente.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <MeuprimeiroComponente />
+    <MeuprimeiroComponente 
+      texto='da csdfcsewfcvjmsdvo fpoaifjm afasf asfacaSFEsf Daniel' 
+      endereco='https://t5z6q4c2.rocketcdn.me/wp-content/uploads/2020/08/sol-afinal-qual-e-sua-cor-conceitos-e-caracteristicas.jpg'
+    
+    />
+
   </main>
 </template>
